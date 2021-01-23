d8d95600c5774a0d67f960998d198f77a1e9e3b1
bbimber <bbimber@gmail.com>
2021-01-23 08:29:45 -0800

    Better error handling

--- R/Seurat_III.R
+++ R/Seurat_III.R
@@ -496,7 +496,7 @@ Find_Markers <- function(seuratObj, identFields, outFile = NULL, testsToUse = c(
 			}, error = function(e){
 				print(paste0('Error running test: ', test))
 				print(conditionMessage(e))
-				print(traceback())
+				traceback()
 				print(utils::str(tMarkers))
 				print(utils::str(seuratObj.markers))
 			})
