636d476c7ae875dc7179e68c409df0bbf257c94a
bbimber <bbimber@gmail.com>
2021-01-25 21:54:54 -0800

    Remove warning

--- tests/testthat/test-doublets.R
+++ tests/testthat/test-doublets.R
@@ -8,14 +8,14 @@ test_that("Doublet detection works as expected", {
   fn <- 'doublets.txt'
   seuratObj <- FindDoublets(seuratObj, rawResultFile = fn)
   
-  expect_equal(17, sum(seuratObj$scDblFinder.class == 'doublet'), tolerance = 5)
-  expect_equal(1540, sum(seuratObj$scDblFinder.class == 'singlet'), tolerance = 5)
+  expect_equal(17, sum(seuratObj$scDblFinder.class == 'doublet'), tolerance = 3)
+  expect_equal(1540, sum(seuratObj$scDblFinder.class == 'singlet'), tolerance = 3)
   
   df <- read.table(fn, header = T, sep = '\t')
   expect_equal(1557, nrow(df))
   unlink(fn)
 
   seuratObj <- FindDoublets(seuratObj, rawResultFile = fn, dropDoublets = TRUE)
-  expect_equal(1540, ncol(seuratObj))
+  expect_equal(1540, ncol(seuratObj), tolerance = 3)
 })
 
