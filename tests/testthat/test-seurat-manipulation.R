eec4d6574a0ea6f7d36c34bdc8ab761003f89fdb
bbimber <bbimber@gmail.com>
2021-02-05 12:32:20 -0800

    Test fix

--- tests/testthat/test-seurat-manipulation.R
+++ tests/testthat/test-seurat-manipulation.R
@@ -23,7 +23,7 @@ test_that("Seurat-manipulation works as expected", {
   seuratList <- SplitSeurat(seuratObj, splitField = 'ClusterNames_0.2', minCellsToKeep = 200)
   expect_equal(3, length(seuratList))
 
-  df <- AvgExpression(seuratObj, groupField = 'ClusterNames_0.2')
+  df <- AvgExpression(seuratObj, groupField = 'ClusterNames_0.4')
   expect_equal(ncol(df), 8)
   expect_equal(nrow(df), nrow(seuratObj) + 2)
   expect_equal(max(df[df$feature == 'LibrarySize', colnames(df) %in% 0:5]), 939113)
