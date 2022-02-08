ab533cb7730b39c92de7e8c1290acf9e79a5dd62
bbimber <bbimber@gmail.com>
2022-02-07 18:56:59 -0800

    Drop slow test

--- tests/testthat/test-cellbender.R
+++ tests/testthat/test-cellbender.R
@@ -1,6 +1,7 @@
-test_that("cellbender works as expected", {
-  dataDir <- '../testdata/CellRanger3/raw_feature_bc_matrix'
-  rawFeatureMatrix <- Seurat::Read10X(data.dir = dataDir, strip.suffix = T)
-  seuratObj <- RunCellBender(rawFeatureMatrix, epochs = 1)
-  print(seuratObj)
-})
+# Disabled since this is very slow w/o GPUs
+# test_that("cellbender works as expected", {
+#   dataDir <- '../testdata/CellRanger3/raw_feature_bc_matrix'
+#   rawFeatureMatrix <- Seurat::Read10X(data.dir = dataDir, strip.suffix = T)
+#   seuratObj <- RunCellBender(rawFeatureMatrix, epochs = 1)
+#   print(seuratObj)
+# })
