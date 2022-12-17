1b42cd4c60825e04a937da8a83612f1f9f6242b9
bbimber <bbimber@gmail.com>
2022-12-17 09:56:49 -0800

    Improve test

--- tests/testthat/test-sda.R
+++ tests/testthat/test-sda.R
@@ -3,7 +3,10 @@ context("scRNAseq")
 test_that("SDA works as expected", {
     seuratObj <- readRDS('../testdata/seuratOutput.rds')
 
-    outputFolder <- tempdir()
+    outputFolder <- paste0(tempdir(), '/sda')
+    if (dir.exists(outputFolder)) {
+        unlink(outputFolder, recursive = TRUE)
+    }
 
     results <- RunSDA(seuratObj, outputFolder = outputFolder, numComps = 5)
     print(results)
