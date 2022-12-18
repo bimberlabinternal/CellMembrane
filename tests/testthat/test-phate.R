b805540b41f452c4a496675367e682864c66a95f
bbimber <bbimber@gmail.com>
2022-12-18 08:23:09 -0800

    Debug phate

--- tests/testthat/test-phate.R
+++ tests/testthat/test-phate.R
@@ -6,9 +6,12 @@ test_that("PHATE works as expected", {
     }
 
     if (!reticulate::py_module_available('phate')) {
+        print('Phate module not found, debugging:')
+        print(reticulate::py_list_packages())
         if ('phate' %in% reticulate::py_list_packages()$package) {
             reticulate::import('phate')
         }
+
         stop('The python phate module has not been installed!')
     }
 
