cf554a5ab7184905475499d826d5a8a427d9fe5f
bbimber <bbimber@gmail.com>
2023-02-01 20:38:57 -0800

    Test improvement

--- tests/testthat/test-lda.R
+++ tests/testthat/test-lda.R
@@ -4,5 +4,9 @@ library(Seurat)
 test_that("LDA works as expected", {
     seuratObj <- readRDS('../testdata/seuratOutput.rds')
 
-    results <- CellMembrane::DoLdaParameterScan(seuratObj, ntopics = c(5, 10))
+    results <- CellMembrane::DoLdaParameterScan(seuratObj, ntopics = c(5, 10, 15))
+    expect_equal(length(results), 3)
+    expect_equal(length(results[[1]]$topic_sums), 5)
+    expect_equal(length(results[[2]]$topic_sums), 10)
+    expect_equal(length(results[[3]]$topic_sums), 15)
 })
