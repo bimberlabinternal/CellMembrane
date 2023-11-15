c03062bb87204be0af19413354aa8f365b67feda
bbimber <bbimber@gmail.com>
2023-11-15 08:32:11 -0800

    Update validation logic

--- R/PseudoBulk.R
+++ R/PseudoBulk.R
@@ -35,9 +35,10 @@ PseudobulkSeurat <- function(seuratObj, groupFields, assays = NULL, additionalFi
   
   metaGrouped <- unique(seuratObj@meta.data[,c('KeyField', groupFields),drop = FALSE])
   rownames(metaGrouped) <- metaGrouped$KeyField
-  if (any(metaGrouped$KeyField != colnames(a))) {
-    x <- metaGrouped$KeyField[metaGrouped$KeyField != colnames(a)]
-    y <- colnames(a)[metaGrouped$KeyField != colnames(a)]
+  if (any(sort(metaGrouped$KeyField) != sort(colnames(a)))) {
+    sel <- sort(metaGrouped$KeyField) != sort(colnames(a))
+    x <- sort(metaGrouped$KeyField[sel])
+    y <- sort(colnames(a)[sel])
 
     stop(paste0('The keyField and AverageExpression object keys to do match. Key fields: ', paste0(x, collapse = ';'), '. Seurat columns: ', paste0(y, collapse = ';')))
   }
