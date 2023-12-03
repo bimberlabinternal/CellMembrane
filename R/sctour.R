83fa9349a346c35a2cac644d8caa97eae208b94e
gboggy2 <boggy@ohsu.edu>
2023-12-02 20:43:36 -0800

    add function Run_sctour to add pseudotime to a Seurat object.

---
+++ R/sctour.R
@@ -0,0 +1,62 @@
+#' @title Run sctour
+#'
+#' @description Runs sctour on a seurat object
+#' @param seuratObj The Seurat object containing the data to be run using sctour
+#' @param GEX_outfile The GEX filename used for output of DropletUtils::write10xCounts (.h5 extension).
+#' @param meta_outfile The filename used for containing the seuratObj metadata.
+#' @param assayName Assay whose data is to be written out with DropletUtils::write10xCounts. Should be "RNA"
+#' @param exclusionList List of genes to be excluded from variable genes in sctour. Usually a RIRA GeneSet.
+#' @param exclusion_JSON_path Filename for the file containing the gene exclusion list (.json extension)
+#' @param ptime_file Filename for the pseudotime results to be written to (.csv extension)
+#' @return A Seurat object with the pseudotime results appended. 
+#' @examples
+#' \dontrun{
+#'   CreateMergedTcrClonotypeFile(seuratObj,
+#'                                outputFile = "./tcrs.csv")
+#'   sctourSeuratObj <- Run_sctour(seuratObj = seuratObj,
+#'                              GEX_outfile = "./GEX.h5",
+#'                              meta_outfile = ./meta.csv, 
+#'                              assayName = "RNA", 
+#'                              exclusionList = RIRA::GetGeneSet('VariableGenes_Exclusion.2'),
+#'                              exclusion_JSON_path = ./exclusionList.json,
+#'                              ptime_file = NULL)
+#' }
+#' @export
+
+Run_sctour <- function(seuratObj=NULL,
+                     GEX_outfile = NULL,
+                     meta_outfile = NULL, 
+                     assayName = "RNA", 
+                     exclusionList = NULL,
+                     exclusion_JSON_path = NULL,
+                     ptime_file = NULL) {
+  
+  # write out data for scTour 
+  write_json(exclusionList, exclusion_JSON_path)
+  DropletUtils::write10xCounts(x = seuratObj@assays[[assayName]]@counts, 
+                               path = GEX_outfile,
+                               overwrite = TRUE)
+  write.csv(seuratObj_clones_NoStim@meta.data, meta_outfile)
+  
+  #copy run_scTour.py in inst/scripts and supply custom arguments 
+  str <- readr::read_file(system.file("scripts/run_scTour.py", package = "CellMembrane"))
+  script <- tempfile()
+  readr::write_file(str, script)
+  
+  newstr <- paste0("run_sctour(GEXfile = '", GEX_outfile,
+                   "',metafile = '", meta_outfile,
+                   "', exclusion_json_path = '", exclusion_JSON_path,
+                   "', ptime_out_file = '", ptime_file,
+                   "')")
+  #write the new arguments into the script and execute
+  readr::write_file(newstr, script, append = TRUE)
+  system2(reticulate::py_exe(), script)
+  
+  pseudotime_values <- read_csv(ptime_file) 
+  pseudotime_values <- pseudotime_values %>% column_to_rownames('...1') 
+  seuratObj <- AddMetaData(seuratObj, pseudotime_values)
+  
+  return(seuratObj)
+}
+
+
---
+++ inst/scripts/run_sctour.py
@@ -0,0 +1,51 @@
+import sctour as sct
+import scanpy as sc
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def run_sctour(GEXfile, metafile, exclusion_json_path, ptime_out_file):
+    adataObj = sc.read_10x_h5(GEXfile)
+    info = pd.read_csv(metafile, sep=',', index_col=0)
+    with open(exclusion_json_path) as f:
+        exclusionList = json.load(f)
+    cells = adataObj.obs_names.intersection(info.index)
+    if len(cells)==0:
+        print("No cells in intersection of adata object and metadata supplied in info. Please ensure these objects are correct.")
+        exit(1)
+    adataObj = adataObj[cells, :]
+    adataObj.obs['ClusterNames_0.2'] = info.loc[cells, 'ClusterNames_0.2'].copy()
+    adataObj.obs['SubjectId'] = info.loc[cells, 'SubjectId'].copy()
+    adataObj.shape
+    adataObj.X = round(adataObj.X).astype(np.float32)
+    adataObj.obs['Population'] = info.loc[cells, 'Population'].copy()
+    adataObj.obs['TandNK_ActivationCore_UCell'] = info.loc[cells, 'TandNK_ActivationCore_UCell'].copy()
+    adataObj.obs['Timepoint'] = info.loc[cells, 'Timepoint'].copy()
+    sc.pp.calculate_qc_metrics(adataObj, percent_top=None, log1p=False, inplace=True)
+    sc.pp.filter_genes(adataObj, min_cells=20)
+    sc.pp.highly_variable_genes(adataObj, flavor='seurat_v3', n_top_genes=2000, subset=True, inplace=False)
+    
+    adataObj = adataObj[:, list(set(adataObj.var_names) - set(exclusionList))]
+    tnode = sct.train.Trainer(adataObj)
+    tnode.train()
+    adataObj.obs['ptime'] = tnode.get_time()
+    mix_zs, zs, pred_zs = tnode.get_latentsp(alpha_z=0.2, alpha_predz=0.8)
+    adataObj.obsm['X_TNODE'] = mix_zs
+    adataObj = adataObj[np.argsort(adataObj.obs['ptime'].values), :]
+    sc.pp.neighbors(adataObj, use_rep='X_TNODE', n_neighbors=15)
+    sc.tl.umap(adataObj, min_dist=0.1)
+    adataObj.obs['ptime'] = sct.train.reverse_time(adataObj.obs['ptime'].values)
+    adataObj.obsm['X_VF'] = tnode.get_vector_field(adataObj.obs['ptime'].values, adataObj.obsm['X_TNODE'])
+    
+    fig, axs = plt.subplots(ncols=4, nrows=1, figsize=(15, 3))
+    adataObj.obs = adataObj.obs.astype({'ClusterNames_0.2':'category', 'SubjectId':'category'})
+    sc.pl.umap(adataObj, color='Timepoint', size=20, ax=axs[0], show=False)
+    sc.pl.umap(adataObj, color='ptime', size=20, ax=axs[1], show=False)
+    sc.pl.umap(adataObj, color='TandNK_ActivationCore_UCell', size=20, ax=axs[2], show=False)
+    sc.pl.umap(adataObj, color='SubjectId', size=20, ax=axs[3], show=False)
+    plt.show()
+
+    df = pd.DataFrame(adataObj.obs['ptime'])
+    df.to_csv(ptime_out_file)
+
+    return adataObj
