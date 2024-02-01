92974de650d3d1e95454f6239bcd3be5202ad059
bbimber <bbimber@gmail.com>
2024-02-01 13:57:26 -0800

    Add the collapse argument for merging

--- R/Preprocessing.R
+++ R/Preprocessing.R
@@ -239,7 +239,13 @@ PerformEmptyDrops <- function(seuratRawData, emptyDropNIters, fdrThreshold=0.001
 				names(geneIds2) <- rownames(seuratObjs[[datasetId]][[assayName]])
 			}
 
-			seuratObj <- merge(x = seuratObj, y = seuratObjs[[datasetId]], project = projectName, merge.data = merge.data)
+			# NOTE: in Seurat 5.x, the default is to rename layers (i.e. counts.1 and counts.2). Collapse=TRUE avoids this.
+			if (utils::packageVersion("SeuratObject") < '5.0.0') {
+				seuratObj <- merge(x = seuratObj, y = seuratObjs[[datasetId]], project = projectName, merge.data = merge.data)
+			} else {
+				seuratObj <- merge(x = seuratObj, y = seuratObjs[[datasetId]], project = projectName, merge.data = merge.data, collapse = TRUE)
+			}
+
 			seuratObjs[[datasetId]] <- NULL
 			if (doGC) {
 				gc()
