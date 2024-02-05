acf4cd307ee8adfa2c42bb7d9f86d427bf5955dd
bbimber <bbimber@gmail.com>
2024-02-05 15:13:28 -0800

    Update test

--- tests/testthat/test-scMetabolism.R
+++ tests/testthat/test-scMetabolism.R
@@ -5,8 +5,8 @@ test_that("scMetabolism works as expected", {
     seuratObj <- RunScMetabolism(seuratObj)
     
     expect_equal(length(rownames(seuratObj@assays$METABOLISM$score)), 85)
-    expect_equal(max(seuratObj@assays$METABOLISM$score$AAACCTGAGCCAGGAT), 0.155963)
+    expect_equal(max(seuratObj@assays$METABOLISM$score$AAACCTGAGCCAGGAT), 0.155963, tolerance = 0.005)
 
     expect_equal(length(rownames(seuratObj@misc$METABOLISM.KEGG)), 85)
-    expect_equal(max(seuratObj@misc$METABOLISM.KEGG$AAACCTGAGCCAGGAT), 0.155963)
+    expect_equal(max(seuratObj@misc$METABOLISM.KEGG$AAACCTGAGCCAGGAT), 0.155963, tolerance = 0.005)
 })
