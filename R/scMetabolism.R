45b1275cea2a4371d174f6e6ea13f6da693715bf
bbimber <bbimber@gmail.com>
2024-02-08 14:11:14 -0800

    Add check to scMetabolism to abort plots if there is no UMAP

--- R/scMetabolism.R
+++ R/scMetabolism.R
@@ -11,12 +11,17 @@
 RunScMetabolism <- function(seuratObj, method = 'AUCell', doImputation = FALSE, metabolismType = 'KEGG', plotVariables = c('ClusterNames_0.2', 'ClusterNames_0.4', 'ClusterNames_0.6')) {
   seuratObj <- scMetabolism::sc.metabolism.Seurat(obj = seuratObj, method = method, imputation = doImputation, metabolism.type = metabolismType)
 
-  # TODO: switch back to built-in function once this is resolved:  https://github.com/wu-yc/scMetabolism/pull/28
-  print(.MakeDimPlot(obj = seuratObj, pathway = "Glycolysis / Gluconeogenesis", dimention.reduction.type = "umap", dimention.reduction.run = F))
-
   # NOTE: the way they store these results as a non-valid assay could be a problem down the line. Consider a different method, such as @misc.
   pathwayData <- seuratObj@assays$METABOLISM$score
   seuratObj@misc[[paste0('METABOLISM.', metabolismType)]] <- pathwayData
+
+  if (!'umap' %in% names(seuratObj@reductions)) {
+    print('No UMAP reduction found, skipping plots')
+    return(seuratObj)
+  }
+
+  # TODO: switch back to built-in function once this is resolved:  https://github.com/wu-yc/scMetabolism/pull/28
+  print(.MakeDimPlot(obj = seuratObj, pathway = "Glycolysis / Gluconeogenesis", dimention.reduction.type = "umap", dimention.reduction.run = F))
 
   input.pathways <- intersect(rownames(pathwayData), c("Glycolysis / Gluconeogenesis", "Oxidative phosphorylation", "Citrate cycle (TCA cycle)"))
   if (length(input.pathways) > 0) {
