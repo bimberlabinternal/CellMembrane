a14a1f776fdda9e8165d3dfe661dfbcddb00cfc0
bbimber <bbimber@gmail.com>
2024-03-07 22:41:10 -0800

    Update hdWGCNA

--- R/hdWGCNA.R
+++ R/hdWGCNA.R
@@ -5,22 +5,23 @@
 #' @param wgcna_name The name of the hdWGCNA experiment
 #' @param groupName The name of the group of interest in the groupBy column
 #' @param groupBy The metadata column containing the cell type info. 
+#' @param moduleConnectivityGroupBy Passed directly to ModuleConnectivity() description
 #' @param geneSelectionApproach The gene selection approach
 #' @param fraction Fraction of cells that a gene needs to be expressed in order to be included
 #' @param sampleGroupingVariables Metadata variables on which to group cells for MetacellsByGroups()
 #' @param reductionName Select the dimensionality reduction to perform KNN on
-#' @import hdWGCNA
+#' @param soft_power Passed directly to hdWGCNA::ConstructNetwork
 #' @return The modified seurat object
 #' @export
-RunHdWGCNA <- function(seuratObj, wgcna_name, groupName, groupBy, sampleGroupingVariables = c('SubjectId', 'CellType'), geneSelectionApproach = 'fraction', fraction = 0.05, reductionName = 'harmony', assayName = 'RNA') {
-  seuratObj <- SetupForWGCNA(
+RunHdWGCNA <- function(seuratObj, wgcna_name, groupName, groupBy, moduleConnectivityGroupBy, sampleGroupingVariables = c('SubjectId', 'CellType'), geneSelectionApproach = 'fraction', fraction = 0.05, reductionName = 'harmony', assayName = 'RNA', soft_power = NULL) {
+  seuratObj <- hdWGCNA::SetupForWGCNA(
     seuratObj,
     gene_select = geneSelectionApproach,
-    fraction = 0.05,
+    fraction = fraction,
     wgcna_name = wgcna_name
   )
 
-  seuratObj <- MetacellsByGroups(
+  seuratObj <- hdWGCNA::MetacellsByGroups(
     seurat_obj = seuratObj,
     group.by = sampleGroupingVariables,
     reduction = reductionName, 
@@ -30,9 +31,9 @@ RunHdWGCNA <- function(seuratObj, wgcna_name, groupName, groupBy, sampleGrouping
   )
 
   # normalize metacell expression matrix:
-  seuratObj <- NormalizeMetacells(seuratObj, verbose = FALSE)
+  seuratObj <- hdWGCNA::NormalizeMetacells(seuratObj, verbose = FALSE)
 
-  seuratObj <- SetDatExpr(
+  seuratObj <- hdWGCNA::SetDatExpr(
     seuratObj,
     group_name = groupName,
     group.by = groupBy,
@@ -40,73 +41,66 @@ RunHdWGCNA <- function(seuratObj, wgcna_name, groupName, groupBy, sampleGrouping
     slot = 'data' 
   )
 
-  seuratObj <- TestSoftPowers(
+  seuratObj <- hdWGCNA::TestSoftPowers(
     seuratObj,
     networkType = 'signed' # you can also use "unsigned" or "signed hybrid"
   )
 
   # plot the results:
-  print(patchwork::wrap_plots(PlotSoftPowers(seuratObj), ncol=2))
+  print(patchwork::wrap_plots(hdWGCNA::PlotSoftPowers(seuratObj), ncol=2))
 
-  power_table <- GetPowerTable(seuratObj)
-  head(power_table)
-
-  seuratObj <- ConstructNetwork(
+  seuratObj <- hdWGCNA::ConstructNetwork(
     seuratObj, 
-    soft_power=9,
-    setDatExpr=FALSE,
-    tom_name = 'INH' # name of the topoligical overlap matrix written to disk
+    soft_power = soft_power,
+    setDatExpr = FALSE,
+    overwrite_tom = TRUE,
+    tom_name = groupName
   )
 
-  PlotDendrogram(seuratObj, main='INH hdWGCNA Dendrogram')
+  tomFile <- paste0(groupName, '_TOM.rda')
+
+  print(hdWGCNA::PlotDendrogram(seuratObj, main = paste0(groupName, ' hdWGCNA Dendrogram')))
 
   # need to run ScaleData first or else harmony throws an error:
-  seuratObj <- ScaleData(seuratObj, features=VariableFeatures(seuratObj))
+  seuratObj <- Seurat::ScaleData(seuratObj, features = Seurat::VariableFeatures(seuratObj), verbose = FALSE)
 
   # compute all MEs in the full single-cell dataset
-  seuratObj <- ModuleEigengenes(
+  seuratObj <- hdWGCNA::ModuleEigengenes(
     seuratObj,
-    group.by.vars="Sample"
+    verbose = FALSE,
+    group.by.vars = sampleGroupingVariables
   )
-
-  # harmonized module eigengenes:
-  hMEs <- GetMEs(seuratObj)
-
-  # module eigengenes:
-  MEs <- GetMEs(seuratObj, harmonized=FALSE)
 
   # compute eigengene-based connectivity (kME):
-  seuratObj <- ModuleConnectivity(
+  seuratObj <- hdWGCNA::ModuleConnectivity(
     seuratObj,
-    group.by = 'cell_type', group_name = 'INH'
-  )
-
-  # rename the modules
-  seuratObj <- ResetModuleNames(
-    seuratObj,
-    new_name = "INH-M"
+    group.by = moduleConnectivityGroupBy,
+    group_name = groupName
   )
 
   # plot genes ranked by kME for each module
-  p <- PlotKMEs(seuratObj, ncol=5)
-  print(p)
+  print(hdWGCNA::PlotKMEs(seuratObj, ncol=5))
 
-  seuratObj <- ModuleExprScore(
+  seuratObj <- hdWGCNA::ModuleExprScore(
     seuratObj,
     n_genes = 25,
-    method='UCell'
+    method = 'UCell'
   )
 
   # make a featureplot of hMEs for each module
-  plot_list <- ModuleFeaturePlot(
+  plot_list <- hdWGCNA::ModuleFeaturePlot(
     seuratObj,
-    features='hMEs', # plot the hMEs
-    order=TRUE # order so the points with highest hMEs are on top
+    features = 'hMEs', # plot the hMEs
+    order = TRUE # order so the points with highest hMEs are on top
   )
 
   # stitch together with patchwork
-  wrap_plots(plot_list, ncol=6)
+  print(patchwork::wrap_plots(plot_list, ncol=6))
 
   # plot module correlagram
-  ModuleCorrelogram(seuratObj)
+  print(hdWGCNA::ModuleCorrelogram(seuratObj))
+
+  unlink(tomFile)
+
+  return(seuratObj)
 }
--- tests/testthat/test-hdWGCNA.R
+++ tests/testthat/test-hdWGCNA.R
@@ -2,6 +2,8 @@ context("scRNAseq")
 
 test_that("hdWGCNA works as expected", {
     seuratObj <- suppressWarnings(Seurat::UpdateSeuratObject(readRDS('../testdata/seuratOutput.rds')))
-    seuratObj <- RunHdWGCNA(seuratObj, groupName = 'S', groupBy = 'Phase', sampleGroupingVariables = c('Phase', 'ClusterNames_0.2'))
+    seuratObj <- RunHdWGCNA(seuratObj, wgcna_name = 'Test', groupName = 'S', groupBy = 'Phase', moduleConnectivityGroupBy = 'Phase', sampleGroupingVariables = c('Phase', 'ClusterNames_0.2'), reductionName = 'umap')
+    
+    expect_equal(max(seuratObj@misc$Test$module_scores), 0.9958171, tolerance = 0.005)
 
 })
