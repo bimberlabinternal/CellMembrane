fa65e5dbcdc57da4f0f2d5f4455e6962170643ac
gwmcelfresh <mcelfreshgw@gmail.com>
2024-03-11 15:58:56 -0700

    add msigdb gene set parsing to RunEscape

--- R/escape.R
+++ R/escape.R
@@ -4,14 +4,24 @@
 #' @param seuratObj A Seurat object.
 #' @param outputAssayName The name of the assay to store results
 #' @param doPlot If true, a FeaturePlot will be printed for each pathway
+#' @param geneSets Which gene sets should be fetched from MSigDB and calculated. Some recommendations in increasing computation time: H (hallmark, 50 gene sets), C8 (scRNASeq cell type markers, 830 gene sets), C2 (curated pathways, 6366 gene sets), GO:BP (GO biological processes, 7658). 
 #' @return The seurat object with results stored in an assay
 #' @export
-RunEscape <- function(seuratObj, outputAssayName = "escape.ssGSEA", doPlot = FALSE) {
-  GS.hallmark <- escape::getGeneSets(library = "H")
-
+RunEscape <- function(seuratObj, outputAssayName = "escape.ssGSEA", doPlot = FALSE, geneSets = c("H")) {
+  
+  ##GO:BP and other hierarchical gene sets require subcategories to be passed with them, so we should parse those individually and concatenate afterwards. I think each of these need to be individually supported, but I haven't confirmed. 
+  #check if all gene sets are non-hierarchical
+  if (all(geneSets %in% c("H", paste0("C", 1:8)))) {
+    GS <- escape::getGeneSets(library = geneSets)
+  } else if ("GO:BP" %in% geneSets) {
+    geneSets <- geneSets[geneSets != "GO:BP"]
+    GS_GO_BP <- escape::getGeneSets(library = "C5", subcategory = "BP")
+    GS <- c(escape::getGeneSets(library = geneSets), GS_GO_BP)
+  }
+  
   seuratObj <- escape::runEscape(seuratObj,
                              method = "ssGSEA",
-                             gene.sets = GS.hallmark,
+                             gene.sets = GS,
                              groups = 5000,
                              min.size = 0,
                              new.assay.name = outputAssayName)
