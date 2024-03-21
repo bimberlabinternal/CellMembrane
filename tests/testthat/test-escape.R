044a451abbf3af7024311cff9c779c39d4665d1e
bbimber <bbimber@gmail.com>
2024-03-20 19:26:00 -0700

    Debug test

--- tests/testthat/test-escape.R
+++ tests/testthat/test-escape.R
@@ -7,7 +7,7 @@ test_that("escape works as expected", {
     testthat::expect_warning(testthat::expect_error(RunEscape(seuratObj, msigdbGeneSets = NULL, doPca = FALSE, customGeneSets = list("FakeGene" = c("FakeGene")))))
     
     #test full functionality
-    seuratObj <- RunEscape(seuratObj, msigdbGeneSets = "H", customGeneSets = list("CD3E" = c("CD3E")), performDimRedux = FALSE)
+    seuratObj <- RunEscape(seuratObj, msigdbGeneSets = "H", customGeneSets = list("CD3E" = c("CD3E")), performDimRedux = TRUE)
     expect_equal(length(rownames(seuratObj@assays$escape.ssGSEA)), 51) #50 hallmark gene sets, 1 custom gene set
     expect_equal(max(seuratObj@assays$escape.ssGSEA$data[1]), 626, tolerance = 0.5)
     
