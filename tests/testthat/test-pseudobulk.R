de8baa5c6c0c75fc85d7a6848139bebd925d6858
GW McElfresh <mcelfreshgw@gmail.com>
2024-03-22 23:47:36 -0700

    update pseudobulking DE tests

--- tests/testthat/test-pseudobulk.R
+++ tests/testthat/test-pseudobulk.R
@@ -33,12 +33,7 @@ test_that("Pseudobulk-based differential expression works", {
   pbmc_pbulk <- PseudobulkSeurat(pbmc_small, groupFields = c("RNA_snn_res.0.8", "letter.idents", "groups", "random_cohort"))
   design <- DesignModelMatrix(pbmc_pbulk, contrast_columns = c("RNA_snn_res.0.8", "letter.idents","random_cohort"), sampleIdCol = "groups")
   fit <- PerformGlmFit(pbmc_pbulk, design = design, test.use = "QLF")
-  pairwise_de_results <- RunPairwiseContrasts(fit, test.use = "QLF", logFC_threshold = 1)
-  testthat::expect_equal(sum(pairwise_de_results$`X0_A_1-X0_A_2`$differential_expression$table$FDR < 0.05), expected = 0) #this contrast should have no significantly differentially expressed genes
-  testthat::expect_equal(sum(pairwise_de_results$`X0_A_1-X1_B_1`$differential_expression$table$FDR < 0.05), expected = 44, tolerance = 1) #this contrast should have 44 genes that pass the FDR threshold.
-  
-  bar_plot <- CreateStudyWideBarPlot(pairwise_de_results = pairwise_de_results, logFC_threshold = 1)
-  testthat::expect_equal(typeof(bar_plot), expected = "list") #ensure that the ggplot was created.
+  testthat::expect_true("coefficients" %in% names(fit))
 })
 
 test_that("Logic gate study design works", {
