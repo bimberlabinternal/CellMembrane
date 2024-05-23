e8c4875de7d7a7e0cad1ee0468552ff1ec85da62
bbimber <bbimber@gmail.com>
2024-05-23 15:05:02 -0700

    Normalize ssGSEA data based on library size

--- R/escape.R
+++ R/escape.R
@@ -35,7 +35,7 @@ RunEscape <- function(seuratObj, outputAssayName = "escape.ssGSEA", doPlot = FAL
     }
     #if the names are not provided in the custom gene sets, we should warn the user but we can label them here.
     if (is.null(names(customGeneSets))){
-      names(customGeneSets) <- paste0("CustomGeneSet", 1:length(customGeneSets))
+      names(customGeneSets) <- paste0("CustomGeneSet", seq_along(customGeneSets))
       warning("The customGeneSets list is unnamed. Naming the genesets with generic names such as: CustomGeneSet1, CustomGeneSet2 ...")
     }
 
@@ -56,6 +56,8 @@ RunEscape <- function(seuratObj, outputAssayName = "escape.ssGSEA", doPlot = FAL
                                  min.size = 0,
                                  assay = assayName,
                                  new.assay.name = outputAssayName)
+
+  seuratObj <- .NormalizeEscape(seuratObj, assayToNormalize = outputAssayName, assayForLibrarySize = assayName)
   
   #you may want to only score a couple of gene sets (e.g. msigdbGeneSets = NULL), in which case you need to determine the pca parameters to ensure it will run. 
   if (doPca) {
@@ -119,6 +121,30 @@ RunEscape <- function(seuratObj, outputAssayName = "escape.ssGSEA", doPlot = FAL
   seuratObj <- Seurat::RunUMAP(seuratObj, dims = dimsToUse, assay = assayName, reduction = pca.reduction.name, reduction.name = umap.reduction.name, reduction.key = umap.reduction.key, verbose = FALSE)
 
   print(DimPlot(seuratObj, reduction = umap.reduction.name))
+
+  return(seuratObj)
+}
+
+.NormalizeEscape <- function(seuratObj, assayToNormalize, assayForLibrarySize = 'RNA', scale.factor = 1e4) {
+  toNormalize <- Seurat::GetAssayData(seuratObj, assayToNormalize, slot = 'counts')
+  assayForLibrarySizeData <- Seurat::GetAssayData(seuratObj, assay = assayForLibrarySize, slot = 'counts')
+
+  if (any(colnames(toNormalize) != colnames(assayForLibrarySize))) {
+    stop(paste0('The assayToNormalize and assayForLibrarySize do not have the same cell names!'))
+  }
+
+  margin <- 2
+  ncells <- dim(x = toNormalize)[margin]
+
+  for (i in seq_len(length.out = ncells)) {
+    x <- toNormalize[, i]
+    librarySize <- sum(assayForLibrarySizeData[, i])
+
+    xnorm <- log1p(x = x / librarySize * scale.factor)
+    toNormalize[, i] <- xnorm
+  }
+
+  seuratObj <- Seurat::SetAssayData(seuratObj, assay = assayToNormalize, slot = 'data', new.data = toNormalize)
 
   return(seuratObj)
 }
