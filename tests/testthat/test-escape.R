7d7aedcfa9e52cbbbd9d918aa1e2fa449666260c
bbimber <bbimber@gmail.com>
2024-05-28 10:33:28 -0700

    Update tests

--- tests/testthat/test-escape.R
+++ tests/testthat/test-escape.R
@@ -10,7 +10,7 @@ test_that("escape works as expected", {
     seuratObj <- RunEscape(seuratObj, msigdbGeneSets = "H", customGeneSets = list("CD3E" = c("CD3E")), performDimRedux = TRUE)
     expect_equal(length(rownames(seuratObj@assays$escape.ssGSEA)), 51) #50 hallmark gene sets, 1 custom gene set
     expect_equal(max(seuratObj@assays$escape.ssGSEA$counts[1]), 626, tolerance = 0.5)
-    expect_equal(max(seuratObj@assays$escape.ssGSEA$data[1]), 0.265, tolerance = 0.5)
+    expect_equal(max(seuratObj@assays$escape.ssGSEA$data[1]), 0.265, tolerance = 0.01)
     
     expect_true('pca.escape.ssGSEA' %in% names(seuratObj@reductions))
     print(names(seuratObj@reductions))
