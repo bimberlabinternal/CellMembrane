90002e75b148d859294934331e8bf1d96bc74297
bbimber <bbimber@gmail.com>
2024-06-05 20:39:49 -0700

    Bugfix

--- inst/scripts/calculate_Diversity.py
+++ inst/scripts/calculate_Diversity.py
@@ -8,9 +8,9 @@ def calculate_Diversity(conga_clones_file, outputFile, order1, order2):
   order1 = int(order1)
   order2 = int(order2)
   temp = generalized_simpsons_entropy(df.loc[df['sampleId'] == sampleId, 'clone_size'], orders=np.arange(order1, order2))
-  temp = temp.add_suffix("_" + sampleId)
+  temp = temp.add_suffix("_" + str(sampleId))
   for sampleId in df.sampleId.unique()[1:]:
     div = generalized_simpsons_entropy(df.loc[df['sampleId'] == sampleId, 'clone_size'], orders=np.arange(order1, order2))
-    div = div.add_suffix("_" + sampleId)
+    div = div.add_suffix("_" + str(sampleId))
     temp = temp.join(div)
   temp.to_csv(outputFile, na_rep="NA")
