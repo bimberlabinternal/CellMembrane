05391d3b4a4247e95bc1e5a906e10eb1b3816fee
GW McElfresh <mcelfreshgw@gmail.com>
2024-06-08 14:26:17 -0700

    separate scoring function, add docs, improve sanity checks

--- R/ssGSEA_Alternatives.R
+++ R/ssGSEA_Alternatives.R
@@ -8,6 +8,7 @@
 #' @param assay The assay to use.
 #' @param layer The layer to use.
 #' @param outputAssayName The name of the assay to store the results in.
+#' @param parallelWorkers The number of parallel workers/cores to use.
 #' @examples 
 #' \dontrun{
 #' # Load the Seurat object
@@ -18,6 +19,7 @@
 #' # Run the alternative ssGSEA
 #' seuratObj <- .AlternativeSsgseaSeurat(seuratObj, geneSets = geneSets)
 #' }
+#' @return A Seurat object with the ssGSEA scores stored in the specified assay.
 
 
 .AlternativeSsgseaSeurat <- function(seuratObj = seuratObj, 
@@ -26,15 +28,41 @@
                                      groups = 5000, 
                                      assay = "RNA", 
                                      layer = 'counts', 
-                                     outputAssayName = "ssGSEA.alternative") {
+                                     outputAssayName = "ssGSEA.alternative", 
+                                     parallelWorkers = 4) {
+  # basic checks for the validity of the Seurat object
+  if (is.null(seuratObj)) {
+    stop('Error: seuratObj not found. Please define a Seurat Object containing a count matrix.')
+  } else if (!(layer %in% SeuratObject::Layers(seuratObj))){
+    stop('Error: Layer ', layer, ' not found in Seurat Object. Please check the layer name.')
+  } else if (!(assay %in% SeuratObject::Assays(seuratObj))){
+    stop('Error: Assay ', assay, ' not found in Seurat Object. Please check the assay name.')
+  }
+  
+  # Check that the gene sets are named lists of vectors
+  if (!is.list(geneSets)) {
+    stop('Error: geneSets must be a named list of gene sets.')
+  } else if (!all(sapply(geneSets, is.vector))) {
+    stop('Error: Non-vector element within geneSets. Each entry within the list must be a vector of gene symbols.')
+  }
+  
   # harvest the count matrix
   countMatrix <- SeuratObject::GetAssayData(seuratObj, layer = layer, assay = assay)
   # create a matrix to store the enrichment scores
   scores <- matrix(0, nrow = length(geneSets), ncol = ncol(countMatrix))
   rownames(scores) <- names(geneSets)
   colnames(scores) <- colnames(countMatrix)
-  # set up the parallel processing plan
-  future::plan("multisession", workers = parallelly::availableCores())
+  
+  # set up the parallel processing plan & check user's number of cpu cores.
+  if (future::availableCores() == 1) {
+    future::plan("sequential", workers = 1 )
+  } else {
+    if (future::availableCores()-1 > parallelWorkers) {
+      message("Further parallelization is possible. If memory pressure/limits permit, consider increasing the parallelWorkers parameter up to a maximum of " , 
+      future::availableCores()-1, ". Current parallelWorkers value is ", parallelWorkers, ".")
+    }
+    future::plan("multisession", workers = min(parallelWorkers, future::availableCores()-1))
+  }
   # Create a progress handler to print a progress bar
   progressr::handlers("txtprogressbar")
   
@@ -47,33 +75,12 @@
       gene_set <- unlist(geneSets[gene_set_name])
       gene_set <- gene_set[gene_set %in% rownames(countMatrix)]
       # 
-      scores <- sapply(colnames(countMatrix), function(sample) {
-        ranked_genes <- rank(-countMatrix[, sample])
-        genes_in_set <- gene_set
-        
-        # Subset the ranked genes for the gene set and all genes
-        ranks_all <- ranked_genes
-        ranks_set <- ranked_genes[genes_in_set]
-        
-        # Compute dts distance using the twosample package
-        if (method == 'dts') {
-          distance <- twosamples::dts_stat(ranks_set, ranks_all)
-        } else if (method %in% c('wass', "wasserstein")) {
-          distance <- twosamples::wass_stat(ranks_set, ranks_all)
-        } else if (method == 'ks') {
-          distance <- twosamples::ks_stat(ranks_set, ranks_all)
-        } else {
-          stop('Method not supported')
-        }
-        
-        # Return the dts distance as the enrichment score
-        distance
-      })
+      scores <- .computeSsgseaScores(gene_set, countMatrix, method)
       names(scores) <- colnames(countMatrix)
       # update the progress bar
       p()
       
-      scores
+      return(scores)
     }, future.seed=1234)
     
     # Convert the list of vectors to a matrix
@@ -83,4 +90,37 @@
   
   seuratObj[[outputAssayName]] <- Seurat::CreateAssayObject(counts = Seurat::as.sparse(scores))
   return(seuratObj)
+}
+
+#' @title Compute ssGSEA scores for a gene set
+#' @description This function computes the ssGSEA scores for a gene set using the twosamples package.
+#' @param gene_set A vector of gene symbols to compute the ssGSEA scores for.
+#' @param countMatrix A matrix of gene expression values where rows are genes and columns are samples.
+#' @param method The method to use for ssGSEA. The dts, wass, and ks methods from the twosamples package are supported.
+#' @return A vector of ssGSEA scores from a gene set for each sample.
+#' 
+.computeSsgseaScores <- function(gene_set, countMatrix, method) {
+  distances <- sapply(colnames(countMatrix), function(sample) {
+  ranked_genes <- rank(-countMatrix[, sample])
+  genes_in_set <- gene_set
+  
+  # Subset the ranked genes for the gene set and all genes
+  ranks_all <- ranked_genes
+  ranks_set <- ranked_genes[genes_in_set]
+  
+  # Compute a distribution distance using the twosamples package
+  if (method == 'dts') {
+    distance <- twosamples::dts_stat(ranks_set, ranks_all)
+  } else if (method %in% c('wass', "wasserstein")) {
+    distance <- twosamples::wass_stat(ranks_set, ranks_all)
+  } else if (method == 'ks') {
+    distance <- twosamples::ks_stat(ranks_set, ranks_all)
+  } else {
+    stop('Method ', method, ' not supported')
+  }
+  
+  # Return the dts distance as the enrichment score
+  distance
+})
+  return(distances)
 }
