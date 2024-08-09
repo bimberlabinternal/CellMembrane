4125285756fb8f5212a8763b1dec9e6aa51935b3
gboggy2 <boggy@ohsu.edu>
2024-08-09 13:16:53 -0700

    Refactor triageADTsAndClassifyCells to rely on subfunctions

--- R/ADTs.R
+++ R/ADTs.R
@@ -19,28 +19,65 @@
 #' @param plots If TRUE, plots will be generated.
 #' @param peakdist_sd_ratio An upper bound of acceptable peak distance divided by the standard deviation of the ADT.
 #' @param peakheight_ratio_threshold The threshold for the peak height ratio.
+#' @param xdist_ratio_threshold The threshold for the peak1_antimode_distance/peak2_antimode_distance ratio.
 #' @export
 
 triageADTsAndClassifyCells <- function(seuratObj,
+                                       whitelist = NULL,
                                        adtwhitelist = NULL, 
                                        assay = "ADT", 
                                        layer = 'data', 
                                        libraryIdentificationColumn = 'cDNA_ID', 
                                        minimumCounts = 200, 
                                        minimumCells = 20, 
-                                       plots = T, 
-                                       whitelist = NULL, 
-                                       peakdist_sd_ratio = 0.1,
-                                       peakheight_ratio_threshold = 50
-){
+                                       plots = T,
+                                       peakdist_sd_ratio = 1,
+                                       peakheight_ratio_threshold = 50,
+                                       xdist_ratio_threshold = 6){
   if (!is.logical(plots)) {
     stop("The 'plots' argument must be TRUE or FALSE.")
   }
   
-  #TODO: If this is going to return a seurat object with calls, it should store the default ident and reset it after running. 
+  df <- .CalculateStatistics(seuratObj, assay, layer, libraryIdentificationColumn,
+                             minimumCounts, minimumCells, plots, adtwhitelist, whitelist,
+                             peakdist_sd_ratio, xdist_ratio_threshold)
+  df <- .TriageADTs(df, minimumCells, peakdist_sd_ratio, peakheight_ratio_threshold, xdist_ratio_threshold)
+  seuratObj <- .ClassifyCells(seuratObj, df, libraryIdentificationColumn, assay, layer, adtwhitelist)
+  
+  return(seuratObj)
+}
+
+#' @title Calculate Statistics for ADT distributions
+#' 
+#' @description This function will calculate metrics and produce a dataframe that will be used for classification by .TriageADTs
+#' 
+#' @param seuratObj A Seurat object.
+#' @param whitelist A vector of library IDs to run the classification on. If NULL, all libraries will be run.
+#' @param adtwhitelist An optional vector of ADTs to be triaged and classified.
+#' @param assay The assay to use.
+#' @param layer The layer to use.
+#' @param libraryIdentificationColumn The column in the metadata that identifies the atomic unit of sequencing data (e.g. cDNA library). 
+#' @param minimumCounts A lower bound on the number of reads in the ADT assay to attempt classification.
+#' @param minimumCells A lower bound on the number of cells recovered from a library to attempt classification.
+#' @param plots If TRUE, plots will be generated.
+#' @param peakdist_sd_ratio An upper bound of acceptable peak distance divided by the standard deviation of the ADT.
+#' @param peakheight_ratio_threshold The threshold for the peak height ratio.
+
+.CalculateStatistics <- function(seuratObj,
+                                 whitelist = NULL,
+                                 adtwhitelist = NULL, 
+                                 assay = "ADT", 
+                                 layer = 'data', 
+                                 libraryIdentificationColumn = 'cDNA_ID', 
+                                 minimumCounts = 200, 
+                                 minimumCells = 20, 
+                                 plots = T,
+                                 peakdist_sd_ratio = 1,
+                                 peakheight_ratio_threshold = 50){
   
   #harvest data from seurat object, and fix Idents to be equal to the libraryIdentificationColumn
   adtMatrix <- Seurat::GetAssayData(seuratObj, assay = assay, layer = layer)
+  countsMatrix <- Seurat::GetAssayData(seuratObj, assay = assay, layer = "counts")
   
   ids <- unique(seuratObj@meta.data[, libraryIdentificationColumn])
   
@@ -52,10 +89,19 @@ triageADTsAndClassifyCells <- function(seuratObj,
     #subset library IDs
     ids <- ids[ids %in% whitelist]
   }
+  
+  if (!is.null(adtwhitelist)){
+    if (!all(adtwhitelist %in% rownames(adtMatrix))) {
+      warning(paste("ADT whitelist values:", paste0(unique(adtwhitelist[!(adtwhitelist %in% rownames(adtMatrix))]), collapse = ", "), "were not found in the available ADTs of the Seurat object. These will be ignored"))
+    }
+    adts <- adtwhitelist[(adtwhitelist %in% rownames(adtMatrix))]
+  }
+  else {
+    adts <- rownames(adtMatrix)
+  }
+  
   #initalize vectors to hold statistics to be used for classification
-  calls <- c()
   cells_vector <- c()
-  decay_vector <- c()
   diff_vector <- c()
   peakheight_ratio_vector <- c()
   peak1_location_vector <- c()
@@ -64,27 +110,19 @@ triageADTsAndClassifyCells <- function(seuratObj,
   peak1_density_vector <- c()
   peak2_density_vector <- c()
   antimode_density_vector <- c()
-  reason_vector <- c()
   cid_vector <- c()
   adt_vector <- c()
+  geom_mean_vector <- c()
+  stdev_vector <- c()
+  med_vector <- c()
+  mod0vec <- c()
+  bandwidth_vec <- c()
   
   for (cid in ids) {
     cells_in_library <- colnames(seuratObj)[seuratObj@meta.data[[libraryIdentificationColumn]] == cid]
     # check library size and skip if too small
-    if (!is.null(adtwhitelist)){
-      if (!all(adtwhitelist %in% rownames(adtMatrix))) {
-        warning(paste("ADT whitelist values:", paste0(unique(adtwhitelist[!(adtwhitelist %in% rownames(adtMatrix))]), collapse = ", "), "were not found in the available ADTs of the Seurat object. These will be ignored"))
-      }
-      adts <- adtwhitelist[(adtwhitelist %in% rownames(adtMatrix))]
-    }
-    else {
-      adts <- rownames(adtMatrix)
-    }
     if (length(cells_in_library) <= minimumCells) {
-      cells_vector <- c(cells_vector, length(cells_in_library))
-      reason_vector <- c(reason_vector, rep("Low Counts", length(adts)))
-      calls <- c(calls, rep("Fail", length(adts)))
-      decay_vector <- c(decay_vector, rep(NA, length(adts)))
+      cells_vector <- c(cells_vector, rep(length(cells_in_library), length(adts)))
       diff_vector <- c(diff_vector, rep(NA, length(adts)))
       peakheight_ratio_vector <- c(peakheight_ratio_vector, rep(NA, length(adts)))
       peak1_location_vector <- c(peak1_location_vector, rep(NA, length(adts)))
@@ -95,19 +133,24 @@ triageADTsAndClassifyCells <- function(seuratObj,
       antimode_density_vector <- c(antimode_density_vector, rep(NA, length(adts)))
       adt_vector <- c(adt_vector, adts)
       cid_vector <- c(cid_vector, rep(cid, length(adts)))
+      geom_mean_vector <- c(geom_mean_vector, rep(NA, length(adts)))
+      stdev_vector <- c(stdev_vector, rep(NA, length(adts)))
+      med_vector <- c(med_vector, rep(NA, length(adts)))
+      mod0vec <- c(mod0vec, rep(NA, length(adts)))
+      bandwidth_vec <- c(bandwidth_vec, rep(NA, length(adts)))
       next
     }
     # Iterate over all ADTs in cDNA_Id
     for (adt in adts) {
-      
       library_adt_vector <- adtMatrix[adt,cells_in_library]
+      library_counts_vector <- countsMatrix[adt, cells_in_library]
       #debugging
       if (plots) {
         hist(library_adt_vector, breaks = 100, main = paste(cid, ", ", adt))
       }
       for (mod0val in seq(2,8,1)) {
         #try to find bimodality using locmodes
-        multimodeResult <- multimode::locmodes(library_adt_vector, mod0 = mod0val, lowsup=0, uppsup = 5, n = 10000, display = plots)
+        multimodeResult <- multimode::locmodes(library_adt_vector, mod0 = mod0val, lowsup=0, uppsup = 10, n = 10000, display = plots)
         #if mutlimode::locmodes succeeds (i.e. two modes and an antimode are not NA values), calculate heuristics
         if (sum(!is.na(multimodeResult$fvalue))>=3) {
           #TODO: sorting isn't really appropriate here, but maybe when Greg can implement his previous code that loops through the mod values, it will be necessary again. 
@@ -122,61 +165,32 @@ triageADTsAndClassifyCells <- function(seuratObj,
           antimode <- multimodeResult$fvalue[antimode_index]
           antimode_location <- multimodeResult$locations[antimode_index]
           peakheight_ratio <- (peak1-antimode)/(peak2-antimode)
-          diff <- ((peak1_location - peak2_location)^2)/sd(library_adt_vector)
+          diff <- ((peak1_location - peak2_location)^2)/(sd(library_adt_vector)^2)
+          bandwidth <- multimodeResult$cbw$bw
           # if a valid solution is found to antimode identification, stop iterating mod0val and accept modes
           if ((diff > peakdist_sd_ratio) & (peakheight_ratio < peakheight_ratio_threshold))  {
             break
           }
-          
-        } else if (sum(!is.na(multimodeResult$fvalue)) < 3 | mod0val < 10) {
-          mod0val <- mod0val + 1
-        }
-        else {
-          #if locmodes failed, set heuristics to NA
+        } else {
+          #if locmodes failed, set measurement values to NA
           peak1 <- NA
           peak2 <- NA
+          peak1_location <- NA
+          peak2_location <- NA
           antimode <- NA
+          antimode_location <- NA
           peakheight_ratio <- NA
-          call <- "Fail"
-          reason <- "Indeterminate"
+          diff <- NA
+          bandwidth <- NA
         }
       }
       
-      #apply bimodality heuristics
-      if (!is.na(peakheight_ratio)) {
-        df <- data.frame(locations = multimodeResult$locations, density = multimodeResult$fvalue) |> 
-          dplyr::arrange(-density)
-        
-        #peak-distance divded by the standard deviation of the ADT
-        diff <- ((df$locations[1] - df$locations[2])^2)/sd(library_adt_vector)
-        if (diff < peakdist_sd_ratio) {
-          call <- "Fail"
-          reason <- "Diff Threshold"
-        } else if (peakheight_ratio > peakheight_ratio_threshold) {
-          call <- "Fail"
-          reason <- "Peak Height Ratio Threshold"
-        } else {
-          call <- "Pass"
-          reason <- "Pass"
-        }
-        
-        if (length(unique(library_adt_vector)) > round(minimumCells/2, 0)) {
-          #independent of locmodes, calculate autocorrelation and fit an exponential to model the decay rate of the autocorrelation wrt lag
-          p_acf <- pacf(quantile(library_adt_vector, probs = seq(0.01,1,0.001)), plot = plots)
-          data <- data.frame(p_acf = p_acf$acf, n = p_acf$lag)
-          
-          #this will warn that we're taking logs of negative numbers, but our primary interest is in the first few lags. After that, the autocorrelation will have smaller magnitudes and not matter nearly as much. 
-          #TODO: consider dropping data after the first lag where p_acf = 0
-          model <- suppressWarnings(lm(log(p_acf) ~ 0 + n, data = data))
-          decay = as.double(coef(model))}
-      } else {
-        decay <- NA
-        call <- "Fail"
-        reason <- "Probable ADT failure"
-      }
+      geom_mean <- exp(mean(log(library_counts_vector + 1)))
+      stdev <- sd(library_adt_vector)
+      med <- median(library_adt_vector)
+      
       #store bimodality statistics
       cells_vector <- c(cells_vector, length(cells_in_library))
-      decay_vector <- c(decay_vector, decay)
       diff_vector <- c(diff_vector, diff)
       peakheight_ratio_vector <- c(peakheight_ratio_vector, peakheight_ratio)
       peak1_density_vector <- c(peak1_density_vector, peak1)
@@ -185,18 +199,19 @@ triageADTsAndClassifyCells <- function(seuratObj,
       peak1_location_vector <- c(peak1_location_vector, peak1_location)
       peak2_location_vector <- c(peak2_location_vector, peak2_location)
       antimode_location_vector <- c(antimode_location_vector, antimode_location)
-      calls <- c(calls, call)
-      reason_vector <- c(reason_vector, reason)
       cid_vector <- c(cid_vector, cid)
       adt_vector <- c(adt_vector, adt)
+      geom_mean_vector <- c(geom_mean_vector, geom_mean)
+      stdev_vector <- c(stdev_vector, stdev)
+      med_vector <- c(med_vector, med)
+      mod0vec <- c(mod0vec, mod0val)
+      bandwidth_vec <- c(bandwidth_vec, bandwidth)
     }
   }
+
   return(data.frame(cDNA_ID = cid_vector,
                     ADT = adt_vector,
-                    call = calls,
-                    reason = reason_vector,
                     cells = cells_vector,
-                    decay = decay_vector, 
                     diff = diff_vector, 
                     peakheight_ratio = peakheight_ratio_vector, 
                     peak1_density = peak1_density_vector, 
@@ -204,6 +219,78 @@ triageADTsAndClassifyCells <- function(seuratObj,
                     antimode_density = antimode_density_vector, 
                     peak1_location = peak1_location_vector, 
                     peak2_location = peak2_location_vector, 
-                    antimode_location = antimode_location_vector))
-  #TODO: use these features to classify cells, potentially returning a seurat object with classified cells. 
+                    antimode_location = antimode_location_vector,
+                    geom_mean = geom_mean_vector,
+                    stdev = stdev_vector,
+                    med = med_vector,
+                    mod0val = mod0vec,
+                    bandwidth = bandwidth_vec))
 }
+
+#' @title Triage ADTs for classification
+#' 
+#' @description This function will take in a dataframe and make calls on bimodality
+#' 
+#' @param df A dataframe resulting from running .CalculateStatistics on a Seurat object
+#' @param minimumCells A lower bound on the number of cells recovered from a library to attempt classification.
+#' @param peakdist_sd_ratio An upper bound of acceptable peak distance divided by the standard deviation of the ADT.
+#' @param peakheight_ratio_threshold The threshold for the peak height ratio.
+#' @param xdist_ratio_threshold The threshold for the peak1_antimode_distance/peak2_antimode_distance ratio.
+
+.TriageADTs <- function(df, minimumCells, peakdist_sd_ratio, peakheight_ratio_threshold, xdist_ratio_threshold){
+  df$xdist_ratio <- (abs(df$peak1_location - df$antimode_location))/(abs(df$peak2_location - df$antimode_location))
+  df$reason <- dplyr::case_when(
+    df$cells < minimumCells ~ "Too few cells",
+    is.na(df$antimode_location) ~ "Probable ADT failure",
+    df$peakheight_ratio > peakheight_ratio_threshold ~ "Peak Height Ratio Threshold",
+    df$diff < peakdist_sd_ratio ~ "Diff Threshold",
+    df$xdist_ratio > xdist_ratio_threshold ~ "Distance Ratio Threshold",
+    .default = "Pass"
+  )
+  
+  df$call <- dplyr::case_when(
+    df$reason == "Pass" ~ "Pass",
+    .default = "Fail"
+  )
+  
+  return(df)
+}
+
+#' @title Classify Cells
+#' 
+#' @description This function will classify cells within a Seurat object based on output from .TriageADTs.
+#' 
+#' @param seuratObj A Seurat object.
+#' @param df A dataframe resulting from running .TriageADTs
+#' @param adtwhitelist An optional vector of ADTs to be triaged and classified.
+#' @param assay The assay to use.
+#' @param layer The layer to use.
+#' @param libraryIdentificationColumn The column in the metadata that identifies the atomic unit of sequencing data (e.g. cDNA library). 
+
+.ClassifyCells <- function(seuratObj, df, libraryIdentificationColumn, assay, layer, adtwhitelist) {
+  adtMatrix <- Seurat::GetAssayData(seuratObj, assay = assay, layer = layer)
+  if (!is.null(adtwhitelist)){
+    if (!all(adtwhitelist %in% rownames(adtMatrix))) {
+      warning(paste("ADT whitelist values:", paste0(unique(adtwhitelist[!(adtwhitelist %in% rownames(adtMatrix))]), collapse = ", "), "were not found in the available ADTs of the Seurat object. These will be ignored"))
+    }
+    adts <- adtwhitelist[(adtwhitelist %in% rownames(adtMatrix))]
+  }
+  else {
+    adts <- rownames(adtMatrix)
+  }
+  df_wider <- tidyr::pivot_wider(df, id_cols = cDNA_ID, names_from = ADT,
+                                 values_from = c(reason, call, antimode_location))
+  meta <- seuratObj@meta.data
+  meta <- merge(meta, df_wider, by = libraryIdentificationColumn)
+  seuratObj <- Seurat::AddMetaData(seuratObj, metadata = meta, col.name = colnames(df_wider |> dplyr::select(-cDNA_ID)))
+  seuratObj <- Seurat::AddMetaData(seuratObj, metadata = data.frame(t(as.matrix(adtMatrix))), col.name = adts)
+  for (adt in adts) {
+    seuratObj[[paste0(adt, "_cellcall")]] <- dplyr::case_when(
+      seuratObj[[adt]] >= seuratObj[[paste0("antimode_location_", adt)]] & seuratObj[[paste0("call_", adt)]] == "Pass" ~ "Positive",
+      seuratObj[[adt]] < seuratObj[[paste0("antimode_location_", adt)]] & seuratObj[[paste0("call_", adt)]] == "Pass" ~ "Negative",
+      seuratObj[[paste0("call_", adt)]] == "Fail" ~ "Failed"
+    )
+  }
+  return(seuratObj)
+}
+
