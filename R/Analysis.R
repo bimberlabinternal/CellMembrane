518351ff22d1ef27ed05bb24eface836e953c431
GW McElfresh <mcelfreshgw@gmail.com>
2024-10-11 13:15:13 -0700

    Update ClusteredDotPlot default colors (#271)
    * Update ClusteredDotPlot colors
    
    * add color comments for hex-codes

--- R/Analysis.R
+++ R/Analysis.R
@@ -567,12 +567,13 @@ ClusteredDotPlot <- function(seuratObj, features, groupFields = "ClusterNames_0.
     as.matrix() |>
     Matrix::t()
   #Establish symmetric color scaling based on the extremes in the heatmap
-  col_RNA = circlize::colorRamp2(c(-max(abs(mat)), 0, max(abs(mat))),
-                                 c(grDevices::hcl.colors(palette = "Blue-Red 2", n = 20)[1],
-                                   "gray85",
-                                   grDevices::hcl.colors(palette = "Blue-Red 2", n = 20)[20]),
+  col_RNA = circlize::colorRamp2(quantile(c(-max(abs(mat)), 0, max(abs(mat)))),
+                                 c("#0000FFFF", #blue
+                                   "#7F53FDFF", #purple (pale)
+                                   "gray90", #very light gray
+                                   "#FF6948FF", #orangered/brick red
+                                   "#FF0000FF"), #red
                                  space = "sRGB")
-  
   pct <- pct[,fullorder]
   
   #Set the heatmap name according to scaling
