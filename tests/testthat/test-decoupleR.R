b8b8832473481dd3e4bbf013ef74c965e80520a3
bbimber <bbimber@gmail.com>
2024-12-22 20:58:33 -0800

    Conditionalize decoupleR test

--- tests/testthat/test-decoupleR.R
+++ tests/testthat/test-decoupleR.R
@@ -1,14 +1,18 @@
 context("scRNAseq")
 
 test_that("RunDecoupleR works as expected", {
-  seuratObj <- suppressWarnings(Seurat::UpdateSeuratObject(readRDS('../testdata/seuratOutput.rds')))
-  seuratObj <- RunDecoupleR(seuratObj)
+  if (version$minor > 4.3) {
+    seuratObj <- suppressWarnings(Seurat::UpdateSeuratObject(readRDS('../testdata/seuratOutput.rds')))
+    seuratObj <- RunDecoupleR(seuratObj)
 
-  expect_true('tfsulm' %in% names(seuratObj@assays))
-  expect_equal(nrow(seuratObj@assays$tfsulm), 726)
-  expect_equal(ncol(seuratObj@assays$tfsulm), ncol(seuratObj))
-  
-  CellMembrane::PlotTfData(seuratObj, groupField = 'Phase')
+    expect_true('tfsulm' %in% names(seuratObj@assays))
+    expect_equal(nrow(seuratObj@assays$tfsulm), 726)
+    expect_equal(ncol(seuratObj@assays$tfsulm), ncol(seuratObj))
+
+    CellMembrane::PlotTfData(seuratObj, groupField = 'Phase')
+  } else {
+    print('Skipping decoupleR tests')
+  }
 })
 
 
