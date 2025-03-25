bbaf0ffc29aa519027c015be6d366661f09b1ac7
bbimber <bbimber@gmail.com>
2025-03-25 12:06:32 -0700

    Refactor LogNormalizeUsingAlternateAssay into nimbleR

--- R/Preprocessing.R
+++ R/Preprocessing.R
@@ -386,62 +386,6 @@ MergeSplitLayers <- function(seuratObj) {
 	stop(paste0('Unable to find matrix file in: ', dataDir, ' or ', dirWithFeatureMatrix))
 }
 
-#' @title LogNormalizeUsingAlternateAssay
-#'
-#' @param seuratObj The seurat object
-#' @param assayToNormalize The name of the assay to normalize
-#' @param assayForLibrarySize The name of the assay from which to derive library sizes. This will be added to the library size of assayToNormalize.
-#' @param scale.factor A scale factor to be applied in normalization
-#' @param maxLibrarySizeRatio This normalization relies on the assumption that the library size of the assay being normalized in negligible relative to the assayForLibrarySize. To verify this holds true, the method will error if librarySize(assayToNormalize)/librarySize(assayForLibrarySize) exceeds this value
-#' @export
-LogNormalizeUsingAlternateAssay <- function(seuratObj, assayToNormalize, assayForLibrarySize = 'RNA', scale.factor = 1e4, maxLibrarySizeRatio = 0.01) {
-	toNormalize <- Seurat::GetAssayData(seuratObj, assayToNormalize, layer = 'counts')
-	assayForLibrarySizeObj <- Seurat::GetAssayData(seuratObj, assay = assayForLibrarySize, layer = 'counts')
-
-	if (any(colnames(toNormalize) != colnames(assayForLibrarySize))) {
-		stop(paste0('The assayToNormalize and assayForLibrarySize do not have the same cell names!'))
-	}
-
-	if (is.null(maxLibrarySizeRatio) || is.na(maxLibrarySizeRatio)) {
-	    maxLibrarySizeRatio <- Inf
-	}
-
-	margin <- 2
-	ncells <- dim(x = toNormalize)[margin]
-
-	start_time <- Sys.time()
-	assayForLibrarySizeData <- Matrix::colSums(assayForLibrarySizeObj)
-	ratios <- unlist(sapply(seq_len(length.out = ncells), function(i){
-		x <- toNormalize[, i]
-		sumX <- sum(x)
-		librarySize <- sumX + assayForLibrarySizeData[i]
-
-    lsr <- (sumX / librarySize)
-		if (lsr > maxLibrarySizeRatio) {
-			stop(paste0('The ratio of library sizes was above maxLibrarySizeRatio for cell: ', colnames(assayForLibrarySizeObj)[i], ', on assay: ', assayToNormalize, '. Ratio was: ', lsr, ' (', sumX, ' / ', librarySize, ')'))
-		}
-
-		xnorm <- log1p(x = x / librarySize * scale.factor)
-		toNormalize[, i] <<- xnorm
-
-		return(lsr)
-	}))
-	end_time <- Sys.time()
-
-	print('Normalization time:')
-	print(end_time - start_time)
-
-	print(ggplot(data.frame(lsr = ratios), aes(x = lsr)) +
-	    geom_histogram() +
-	    ggtitle(paste0("Library size ratios: ", assayToNormalize)) +
-        egg::theme_article()
-	)
-
-	seuratObj <- Seurat::SetAssayData(seuratObj, assay = assayToNormalize, layer = 'data', new.data = toNormalize)
-
-	return(seuratObj)
-}
-
 .FindLayersToJoin <- function(seuratObj, assayName) {
 	layerNames <- SeuratObject::Layers(seuratObj, assay = assayName)
 	layerBasenames <- sapply(layerNames, function(x){
--- tests/testthat/test-seurat.R
+++ tests/testthat/test-seurat.R
@@ -141,21 +141,3 @@ test_that("Seurat CellCycleScoring_UCell", {
   expect_equal(sum(seuratObj$PhaseUCell == 'G2M'), 272)
   expect_equal(sum(seuratObj$PhaseUCell == 'G1'), 892)
 })
-
-
-test_that("LogNormalizeUsingAlternateAssay works as expected", {
-  seuratObj <- suppressWarnings(Seurat::UpdateSeuratObject(readRDS('../testdata/seuratOutput.rds')))
-
-  assayToAdd <- Seurat::GetAssayData(seuratObj, assay = 'RNA', layer = 'counts')
-  assayToAdd <- floor(assayToAdd[1:10,] / 5)
-  
-  rownames(assayToAdd) <- paste0('Feature', LETTERS[1:10])
-
-  seuratObj[['Norm']] <- Seurat::CreateAssayObject(assayToAdd)
-
-  seuratObj <- LogNormalizeUsingAlternateAssay(seuratObj, assayToNormalize = 'Norm', assayForLibrarySize = 'RNA')
-
-  nd <- Seurat::GetAssayData(seuratObj, assay = 'Norm', layer = 'data')
-  expect_equal(max(nd[,4]), 3.442982, tolerance = 0.000001)
-  expect_equal(max(nd[,101]), 2.823479, tolerance = 0.000001)
-})
