0de3d7770d41a2126a20929b8537f82548daaaf3
bbimber <bbimber@gmail.com>
2025-03-31 16:12:26 -0700

    Updates for escape

--- tests/testthat/test-escape.R
+++ tests/testthat/test-escape.R
@@ -8,14 +8,16 @@ test_that("escape works as expected", {
     
     #test full functionality
     seuratObj <- RunEscape(seuratObj, msigdbGeneSets = "H", customGeneSets = list("CD3E" = c("CD3E")), performDimRedux = TRUE)
-    expect_equal(length(rownames(seuratObj@assays$escape.ssGSEA)), 51) #50 hallmark gene sets, 1 custom gene set
-    expect_equal(max(seuratObj@assays$escape.ssGSEA$counts[1]), 263, tolerance = 0.5)
-    expect_equal(max(seuratObj@assays$escape.ssGSEA$data[1]), 0.112, tolerance = 0.01)
-    expect_equal(max(seuratObj@assays$escape.ssGSEA$scale.data[1]), -0.624, tolerance = 0.01)
+    expect_equal(length(rownames(seuratObj@assays$escape.H)), 50)
+    expect_equal(length(rownames(seuratObj@assays$escape.CustomGeneSet)), 1)
+
+    expect_equal(max(seuratObj@assays$escape.H$counts[1]), 263, tolerance = 0.5)
+    expect_equal(max(seuratObj@assays$escape.H$data[1]), 0.112, tolerance = 0.01)
+    expect_equal(max(seuratObj@assays$escape.H$scale.data[1]), -0.624, tolerance = 0.01)
     
-    expect_true('pca.escape.ssGSEA' %in% names(seuratObj@reductions))
+    expect_true('pca.escape.H' %in% names(seuratObj@reductions))
     print(names(seuratObj@reductions))
-    expect_true('escape.ssGSEA.umap' %in% names(seuratObj@reductions))
-    expect_true('escape.ssGSEA.nn' %in% names(seuratObj@graphs))
+    expect_true('escape.H.umap' %in% names(seuratObj@reductions))
+    expect_true('escape.H.nn' %in% names(seuratObj@graphs))
     print(names(seuratObj@graphs))
 })
